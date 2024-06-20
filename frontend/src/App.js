55e92a4a8a19c4e1f465ed4e7062faa4f5073051
Aastik <aastik243@gmail.com>
2024-06-20 18:14:33 +0530

    developed frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,28 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';import HomePage from "./pages/home.js";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from "./pages/auth/login/login.js";
 import SignupPage from "./pages/auth/signup/signup.js";
 import HomePage from './pages/home.js';
 import MessagesPage from './pages/messages.js';
 
 import './App.css';
+import Sidebar from './components/common/Sidebar.js';
+import RightPanel from './components/common/RightPanel.js';
 
 
 function App() {
   return (
-    <Router>
+    
       <div className="flex max-w-6xl mx-auto">
+      <Sidebar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path='/messages' element={<MessagesPage />} />
         </Routes>
+        <RightPanel />
       </div>
-    </Router>
+    
   );
 }
 
